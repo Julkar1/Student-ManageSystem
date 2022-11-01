81eba747f303df9d1475f692af3205111c4bf3e4
Julkar Nain <julkar929@gmail.com>
2022-11-02 02:49:25 +0600

    create forms and update urls path

--- students/urls.py
+++ students/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>', views.view_student, name='view_student'),  #create at the time of view_student at views.py
+    path('add/', views.add, name='add'),
+    path('edit/<int:id>/', views.edit, name='edit'),
+    path('delete/<int:id>/', views.delete, name='delete'),
 ]
