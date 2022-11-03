3a9a7d820ceca15d61b7afb3ae2a3e8993cad24c
Julkar Nain <julkar929@gmail.com>
2022-11-04 00:41:09 +0600

    guinocorn installed

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 release: python manage.py migrate
 web: gunicorn djangoMS.wsgi
-heroku ps:scale web=1
--- djangoMS/settings.py
+++ djangoMS/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'students',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'djangoMS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates"),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
